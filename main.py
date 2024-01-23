dda313a14a39b2b32fccbbb2ab3683b27349c4a0
pkoutras <petros.koutras6@gmail.com>
2024-01-23 15:37:30 +0000

    Allow trained model to be loaded in cpu/gpu

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ if __name__ == '__main__':
 
     if opt.resume_path:
         print('loading checkpoint {}'.format(opt.resume_path))
-        checkpoint = torch.load(opt.resume_path)
+        checkpoint = torch.load(opt.resume_path, map_location=lambda storage, loc: storage)
         assert opt.arch == checkpoint['arch']
 
         opt.begin_epoch = checkpoint['epoch']
